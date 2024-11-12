c7891bc95b741d880457ebe776a3207d2919b4db
Loc-nguyen1685 <tanloc1680@gmail.com>
2024-11-12 10:40:30 +0700

    Add files via upload

---
+++ loc.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+	char MSSV[20];
+	char Hoten[80];
+	int Ngaysinh, Thangsinh, Namsinh;
+	float Dxt [20];
+
+int main(){
+	
+	printf("\n nhap ma so sinh vien: ");
+	fgets(MSSV, sizeof(MSSV),stdin);
+	printf("\n ho ten");
+	fgets(Hoten, sizeof(Hoten), stdin);
+}
